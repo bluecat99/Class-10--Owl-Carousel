974e005726c0c5b714d7a6d71374b34b40545109
Md Rakibul Islam Rumman <75201743+bluecat99@users.noreply.github.com>
2020-12-10 11:24:23 +0600

    Add files via upload

---
+++ Owl/js/script.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    smartSpeed:3000,
+    fluidSpeed:2000,
+    nav:true,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
